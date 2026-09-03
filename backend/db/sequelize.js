cf7bb9425cd4eb979cf09973064651a86cf15390
dev.rohozhyn <dev.rohozhyn@gmail.com>
2026-09-03 21:36:38 +0200

    fix test env

--- backend/db/sequelize.js
+++ backend/db/sequelize.js
@@ -1,6 +1,8 @@
 import { Sequelize } from "sequelize";
 
-const isProduction = process.env.NODE_ENV === "production";
+const env = process.env.NODE_ENV || "development";
+const isProduction = env === "production";
+const isTest = env === "test";
 
 const connectionParams = isProduction
   ? {
@@ -15,13 +17,22 @@ const connectionParams = isProduction
         rejectUnauthorized: false,
       },
     }
+  : isTest
+  ? {
+      dialect: process.env.DATABASE_DIALECT || "postgres",
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      host: process.env.DATABASE_HOST,
+      database: process.env.DATABASE_NAME,
+      port: process.env.DATABASE_PORT || 5432,
+    }
   : {
-      dialect: process.env.DATABASE_DIALECT_DEV,
+      dialect: process.env.DATABASE_DIALECT_DEV || "postgres",
       username: process.env.DATABASE_USERNAME_DEV,
       password: process.env.DATABASE_PASSWORD_DEV,
       host: process.env.DATABASE_HOST_DEV,
       database: process.env.DATABASE_NAME_DEV,
-      port: process.env.DATABASE_PORT_DEV,
+      port: process.env.DATABASE_PORT_DEV || 5432,
     };
 
 const sequelize = new Sequelize({
